bc5b864300e0108e81ebce542b4a9f947a1f1848
Khalilur Rahman Faysal <117637433+Faysal890@users.noreply.github.com>
2025-02-14 01:47:18 +0600

    feat: added user model

---
+++ backend/CosmoVerse/CosmoVerse/Data/UserDbContext.cs
@@ -0,0 +1,14 @@
+﻿using CosmoVerse.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace CosmoVerse.Data
+{
+    public class UserDbContext : DbContext
+    {
+        public UserDbContext(DbContextOptions<UserDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
