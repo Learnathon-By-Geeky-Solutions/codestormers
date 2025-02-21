46a8e0f775ae3dc8df52b3647e830f8d47065c7d
Khalilur Rahman Faysal <117637433+Faysal890@users.noreply.github.com>
2025-02-22 01:39:11 +0600

    added validations

--- backend/CosmoVerse/CosmoVerse/Models/Dto/PasswordResetDto.cs
+++ backend/CosmoVerse/CosmoVerse/Models/Dto/PasswordResetDto.cs
@@ -1,9 +1,17 @@
-﻿namespace CosmoVerse.Models.Dto
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CosmoVerse.Models.Dto
 {
     public class PasswordResetDto
     {
+        [Required]
+        [MaxLength(256)]
         public string Email { get; set; } = string.Empty;
+        [Required]
+        [MaxLength(100)]
         public string Token { get; set; } = string.Empty;
+        [Required]
+        [StringLength(100, MinimumLength = 8)]
         public string NewPassword { get; set; } = string.Empty;
     }
 }
--- backend/CosmoVerse/CosmoVerse/Models/Dto/RefreshTokenRequestDto.cs
+++ backend/CosmoVerse/CosmoVerse/Models/Dto/RefreshTokenRequestDto.cs
@@ -1,8 +1,12 @@
-﻿namespace CosmoVerse.Models.Dto
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CosmoVerse.Models.Dto
 {
     public class RefreshTokenRequestDto
     {
+        [Required]
         public Guid Id { get; set; }
+        [Required]
         public required string RefreshToken { get; set; }
     }
 }
